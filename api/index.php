3a409e8b9b3a30557835c60ac5ce7c527281eade
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-04-03 18:07:12 +0800

    frontend folder moved 6-06 4-3-24

--- api/index.php
+++ api/index.php
@@ -1,13 +1,15 @@
 <?php
 /*****************************
-docs.paxagency.com/php V 1.8.0 
-mit license • by paxagency
+docs.paxagency.com/php V 1.7.0 
+mit license • by albert kiteck
 *****************************/
 define('APP_NAME', 'Project');
 define('APP_VERSION', '0.0.1');
 define('APP_LICENSE', 'MIT');
-define('APP_AUTHOR', 'John Smith');
+define('APP_AUTHOR', 'Revance');
 
-$_DIR = getcwd()."/";
+$_DIR = "/var/task/user/";
+
+require_once($_DIR.'api/public.php'); 
 require_once($_DIR.'back/sys/init.php'); 
 ?>
--- src/base_url.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Get host and URI
-$host = $_SERVER['HTTP_HOST'];
-$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$src = '/src';
-?>
